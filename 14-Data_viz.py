73095918c7200bac7eba182b6c32b08830f0eb00
Waqar_Ahmad <waqarafridi54@gmail.com>
2022-12-03 22:59:52 +0500

    my second commit

---
+++ 14-Data_viz.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(style="ticks", color_codes=True)
+titanic=sns.load_dataset("titanic")
+print(titanic)
+
+# sns.countplot(x="sex",data=titanic)
+# plt.show()
+
+# sns.countplot(x="sex",hue="class",data=titanic)
+# plt.show()
+
+p=sns.countplot(x="sex",hue="class",data=titanic)
+p.set_title("plot for basic count plot")
+plt.show()
