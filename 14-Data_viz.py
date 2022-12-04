b0b69db98e47843c1fc35bf825cff957fd024410
Waqar Afridi <84533026+Waqar-Afridi@users.noreply.github.com>
2022-12-04 09:39:03 +0500

    we add new comment in data viz file

--- 14-Data_viz.py
+++ 14-Data_viz.py
@@ -14,3 +14,4 @@ p=sns.countplot(x="sex",hue="class",data=titanic)
 p.set_title("plot for basic count plot")
 plt.show()
 # to edit this file we pull it
+# to to update file in github we need to commmit again 
